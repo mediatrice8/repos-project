1f66813bed0e009160cfdde51a29e6fd2e8840bb
mediatrice8 <kampiremediatrice@gmail.com>
2019-08-31 22:32:33 +0200

    i create class

---
+++ search-app/src/app/user.ts
@@ -0,0 +1,11 @@
+export class User {
+    constructor(public name:string,
+         public login: string,
+          public avatar_url:string,
+           public public_repos:number ,
+            public html_url: string,
+            public created_at:Date, 
+            public followers:number ,
+            public following:number){}
+    
+}
